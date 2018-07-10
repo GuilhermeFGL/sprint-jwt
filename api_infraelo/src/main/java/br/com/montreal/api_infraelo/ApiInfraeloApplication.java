fcf0dabdeb2b44b25def823861b443b37f0fd6e2
GuilhermeFGL <guilherme_fgl@hotmail.com>
2018-07-10 09:51:00 -0300

    init

---
+++ api_infraelo/src/main/java/br/com/montreal/api_infraelo/ApiInfraeloApplication.java
@@ -0,0 +1,12 @@
+package br.com.montreal.api_infraelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiInfraeloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiInfraeloApplication.class, args);
+	}
+}
